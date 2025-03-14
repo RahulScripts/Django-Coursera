884ede62f09e3ae5a880b0f1a6fdd461b8a4f96b
RahulScripts <hallirahul777@gmail.com>
2025-03-14 10:37:53 +0530

    Re-organized all folders, added FullStack_Project

---
+++ FullStack_Project/tradingbot/tradingbotweb/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Currency, Transaction, CurrencyHistory, CurrencyBalance, ExchangeGoal
+
+class TransactionAdmin(admin.ModelAdmin):
+    readonly_fields = ['exchange_date',]
+
+class CurrencyHistoryAdmin(admin.ModelAdmin):
+    readonly_fields=['created_at',]
+# Register your models here.
+admin.site.register(Currency)
+admin.site.register(Transaction, TransactionAdmin)
+admin.site.register(CurrencyHistory, CurrencyHistoryAdmin)
+admin.site.register(CurrencyBalance, CurrencyHistoryAdmin)
+admin.site.register(ExchangeGoal)
